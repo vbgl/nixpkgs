b18cede323c80f77e892db73dab70afb5c884034
Vincent Laporte <Vincent.Laporte@gmail.com>
2017-11-14 23:38:56 +0000

    ocamlPackages.sqlexpr: 0.5.5 -> 0.8.0

--- pkgs/development/ocaml-modules/sqlexpr/default.nix
+++ pkgs/development/ocaml-modules/sqlexpr/default.nix
@@ -1,15 +1,22 @@
-{ stdenv, buildOcaml, fetchurl, batteries, csv, ocaml_lwt, ocaml_sqlite3, estring }:
+{ stdenv, buildOcaml, ocaml, fetchurl, cppo, csv, ocaml_lwt, ppx_tools, re, ocaml_sqlite3 }:
+
+if !stdenv.lib.versionAtLeast ocaml.version "4.02"
+then throw "sqlexpr is not available for OCaml ${ocaml.version}"
+else
 
 buildOcaml rec {
   name = "sqlexpr";
-  version = "0.5.5";
+  version = "0.8.0";
 
   src = fetchurl {
-    url = "https://forge.ocamlcore.org/frs/download.php/1203/ocaml-sqlexpr-${version}.tar.gz";
-    sha256 = "02pi0xxr3xzalwpvcaq96k57wz2vxj20l2mga1a4d2ddvhran8kr";
+    url = "https://github.com/mfp/ocaml-sqlexpr/releases/download/0.8.0/ocaml-sqlexpr-0.8.0.tar.gz";
+    sha256 = "0v1wavqzjnjd3v7z398rfmvpbavzb56i1qrnhjkcsg458jr5nbkp";
   };
 
-  propagatedBuildInputs = [ batteries csv ocaml_lwt ocaml_sqlite3 estring ];
+  configurePhase = "ocaml setup.ml -configure --prefix $out --disable-camlp4";
+
+  buildInputs = [ cppo ppx_tools re ];
+  propagatedBuildInputs = [ csv ocaml_lwt ocaml_sqlite3 ];
 
   meta = with stdenv.lib; {
     homepage = https://github.com/mfp/ocaml-sqlexpr;
