455bac38d0e6e54757f613969cf32b8ddb20f8c8
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-03-31 14:38:12 +0200

    RIO

--- pkgs/development/ocaml-modules/phylogenetics/default.nix
+++ pkgs/development/ocaml-modules/phylogenetics/default.nix
@@ -37,11 +37,6 @@ buildDunePackage rec {
     printbox-text
   ];
 
-  checkPhase = ''
-    runHook preCheck
-    dune build @app/fulltest
-    runHook postCheck
-  '';
   doCheck = true;
 
   meta = with lib; {
