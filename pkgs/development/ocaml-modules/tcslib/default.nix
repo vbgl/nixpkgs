44d590117f450a373ab71a3369d2c12376cd9d07
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-07-25 08:50:33 +0200

    ocamlPackages.tcslib: fix for OCaml 5.0

--- pkgs/development/ocaml-modules/tcslib/default.nix
+++ pkgs/development/ocaml-modules/tcslib/default.nix
@@ -1,10 +1,11 @@
-{ lib, fetchFromGitHub, ocamlPackages, buildOasisPackage, extlib, num }:
+{ lib, fetchFromGitHub, stdenv, ocaml, findlib, ocamlbuild, camlp-streams, extlib, num }:
 
-buildOasisPackage rec {
-  pname = "tcslib";
+lib.throwIf (lib.versionOlder ocaml.version "4.03")
+  "tcslib is not available for OCaml ${ocaml.version}"
+
+stdenv.mkDerivation rec {
+  pname = "ocaml${ocaml.version}-tcslib";
   version = "0.3";
-
-  minimumOCamlVersion = "4.03.0";
 
   src = fetchFromGitHub {
     owner  = "tcsprojects";
@@ -12,6 +13,29 @@ buildOasisPackage rec {
     rev    = "v${version}";
     sha256 = "05g6m82blsccq8wx8knxv6a5fzww7hi624jx91f9h87nk2fsplhi";
   };
+
+  postPatch = lib.optionalString (lib.versionAtLeast ocaml.version "4.07") ''
+    substituteInPlace src/data/tcsset.ml --replace Pervasives. Stdlib.
+  '';
+
+  strictDeps = true;
+
+  nativeBuildInputs = [ ocaml ocamlbuild findlib ];
+  buildInputs = [ camlp-streams ocamlbuild ];
+
+  buildPhase = ''
+    runHook preBuild
+    ocamlbuild -use-ocamlfind -plugin-tag 'package(camlp-streams)' src/TCSLib.cma src/TCSLib.cmxs
+    runHook postBuild
+  '';
+
+  createFindlibDestdir = true;
+
+  installPhase = ''
+    runHook preInstall
+    find _build/src -name '*.cm[aitx]*' | xargs ocamlfind install TCSLib src/META _build/src/TCSLib.a
+    runHook postInstall
+  '';
 
   propagatedBuildInputs = [ extlib num ];
 
