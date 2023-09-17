b2d90f0b2dbb06a4fc33ef853899965e8cbe9712
Vincent Laporte <Vincent.Laporte@gmail.com>
2023-09-17 18:48:20 +0200

    ocamlPackages.camomile: enable version 2.0.0

--- pkgs/development/ocaml-modules/camomile/default.nix
+++ pkgs/development/ocaml-modules/camomile/default.nix
@@ -1,34 +1,54 @@
-{ lib, fetchFromGitHub, buildDunePackage, ocaml, cppo }:
+{ lib, fetchFromGitHub, buildDunePackage, ocaml, cppo
+, camlp-streams, dune-site
+, version ? "1.0.2"
+}:
 
-buildDunePackage rec {
+let params =
+  {
+    "1.0.2" = {
+      src = fetchFromGitHub {
+        owner = "yoriyuki";
+        repo = "camomile";
+        rev = version;
+        sha256 = "00i910qjv6bpk0nkafp5fg97isqas0bwjf7m6rz11rsxilpalzad";
+      };
+
+      nativeBuildInputs = [ cppo ];
+
+      configurePhase = ''
+        runHook preConfigure
+        ocaml configure.ml --share $out/share/camomile
+        runHook postConfigure
+      '';
+
+      postInstall = ''
+        echo "version = \"${version}\"" >> $out/lib/ocaml/${ocaml.version}/site-lib/camomile/META
+      '';
+
+    };
+
+    "2.0.0" = {
+      src = fetchFromGitHub {
+        owner = "ocaml-community";
+        repo = "camomile";
+        rev = "v${version}";
+        hash = "sha256-HklX+VPD0Ta3Knv++dBT2rhsDSlDRH90k4Cj1YtWIa8=";
+      };
+
+      propagatedBuildInputs = [ camlp-streams dune-site ];
+    };
+  }
+; in
+
+
+buildDunePackage (params."${version}" // {
   pname = "camomile";
-  version = "1.0.2";
-
-  useDune2 = true;
-
-  src = fetchFromGitHub {
-    owner = "yoriyuki";
-    repo = pname;
-    rev = version;
-    sha256 = "00i910qjv6bpk0nkafp5fg97isqas0bwjf7m6rz11rsxilpalzad";
-  };
-
-  nativeBuildInputs = [ cppo ];
-
-  configurePhase = ''
-    runHook preConfigure
-    ocaml configure.ml --share $out/share/camomile
-    runHook postConfigure
-  '';
-
-  postInstall = ''
-    echo "version = \"${version}\"" >> $out/lib/ocaml/${ocaml.version}/site-lib/camomile/META
-  '';
+  inherit version;
 
   meta = {
-    inherit (src.meta) homepage;
+    homepage = "https://github.com/ocaml-community/Camomile";
     maintainers = [ lib.maintainers.vbgl ];
     license = lib.licenses.lgpl21;
     description = "A Unicode library for OCaml";
   };
-}
+})
