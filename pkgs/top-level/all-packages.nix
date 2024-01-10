f3e3168333f06a4b41e107a9db8e227e46cb6606
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-01-10 06:35:53 +0100

    heptagon: use OCaml 4.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -16389,7 +16389,9 @@ with pkgs;
     inherit (emacs.pkgs.melpaStablePackages) irony;
   };
 
-  heptagon = callPackage ../development/compilers/heptagon { };
+  heptagon = callPackage ../development/compilers/heptagon {
+    ocamlPackages = ocaml-ng.ocamlPackages_4_14;
+  };
 
   holo-build = callPackage ../tools/package-management/holo-build { };
 
