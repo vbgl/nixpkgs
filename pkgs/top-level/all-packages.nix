fff2983c925c3347b16737a53a86e6c3909f210c
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-01-10 06:36:17 +0100

    orpie: use OCaml 4.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -34485,7 +34485,9 @@ with pkgs;
 
   opusTools = callPackage ../applications/audio/opus-tools { };
 
-  orpie = callPackage ../applications/misc/orpie { };
+  orpie = callPackage ../applications/misc/orpie {
+    ocamlPackages = ocaml-ng.ocamlPackages_4_14;
+  };
 
   osmo = callPackage ../applications/office/osmo { };
 
