1b1e666a24359a3803d5862df7f53f54e882b9b3
Vincent Laporte <Vincent.Laporte@gmail.com>
2024-01-18 19:43:25 +0100

    ocamlPackages.trace: 0.5 → 0.6

--- pkgs/development/ocaml-modules/trace/default.nix
+++ pkgs/development/ocaml-modules/trace/default.nix
@@ -1,15 +1,17 @@
-{ lib, fetchurl, buildDunePackage }:
+{ lib, fetchurl, buildDunePackage, ocaml, mtime }:
 
 buildDunePackage rec {
   pname = "trace";
-  version = "0.5";
+  version = "0.6";
 
   minimalOCamlVersion = "4.07";
 
   src = fetchurl {
     url = "https://github.com/c-cube/ocaml-trace/releases/download/v${version}/trace-${version}.tbz";
-    hash = "sha256-l0NvWPGBd1WR+b50WXEYfptuCUjda8MlZ/o5YngRNIg=";
+    hash = "sha256-l8xBWblkKa3AGoS/8e00uPBnRrxWpAohSmMGA2vi3zg=";
   };
+
+  propagatedBuildInputs = lib.optional (lib.versionAtLeast ocaml.version "4.08") mtime;
 
   meta = {
     description = "Common interface for tracing/instrumentation libraries in OCaml";
