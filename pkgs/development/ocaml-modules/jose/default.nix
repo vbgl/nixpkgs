d59e62d5c716494b4ced824b6220f56bdacc1340
Marijan Petričević <marijan.petricevic94@gmail.com>
2025-05-02 16:37:48 -0500

    ocamlPackages.jose: init at 0.10.0

---
+++ pkgs/development/ocaml-modules/jose/default.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildDunePackage,
+  fetchurl,
+  containers,
+  junit_alcotest,
+  base64,
+  x509,
+  yojson,
+  zarith,
+}:
+
+buildDunePackage rec {
+  pname = "jose";
+  version = "0.10.0";
+
+  src = fetchurl {
+    url = "https://github.com/ulrikstrid/ocaml-jose/releases/download/v${version}/${pname}-${version}.tbz";
+    hash = "sha256-F6Odq5JXTkAxdqV3HQusoF+9rvt4BZytslKnsIjJLI8=";
+  };
+
+  propagatedBuildInputs = [
+    base64
+    x509
+    yojson
+    zarith
+  ];
+
+  doCheck = true;
+  checkInputs = [
+    containers
+    junit_alcotest
+  ];
+
+  meta = {
+    description = "JOSE specification implementation in OCaml";
+    homepage = "https://github.com/ulrikstrid/ocaml-jose";
+    license = lib.licenses.mit;
+  };
+}
