8fb14d672dc6f254843691bd5dfddd97f50d8b5f
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-04 22:48:54 -0400

    ocamlPackages.smtml: init at 0.7.0
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1943,6 +1943,8 @@ let
 
         slug = callPackage ../development/ocaml-modules/slug { };
 
+        smtml = callPackage ../development/ocaml-modules/smtml { };
+
         sodium = callPackage ../development/ocaml-modules/sodium { };
 
         sosa = callPackage ../development/ocaml-modules/sosa { };
