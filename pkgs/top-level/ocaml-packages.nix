a12742875260a9796dbd25903b9c9985e019d9d6
Ethan Carter Edwards <ethan@ethancedwards.com>
2025-06-06 15:34:51 -0400

    ocamlPackages.processor: init at 0.1
    Signed-off-by: Ethan Carter Edwards <ethan@ethancedwards.com>

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1706,6 +1706,8 @@ let
 
         process = callPackage ../development/ocaml-modules/process { };
 
+        processor = callPackage ../development/ocaml-modules/processor { };
+
         prometheus = callPackage ../development/ocaml-modules/prometheus { };
 
         progress = callPackage ../development/ocaml-modules/progress { };
