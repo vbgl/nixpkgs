b66901831fbbeac9ea09e4e41dc8e944e38f5cf6
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-07-15 06:25:23 +0200

    ocamlPackages.stdlib-random: init at 1.2.0

--- pkgs/top-level/ocaml-packages.nix
+++ pkgs/top-level/ocaml-packages.nix
@@ -1930,6 +1930,8 @@ let
 
         stdint = callPackage ../development/ocaml-modules/stdint { };
 
+        stdlib-random = callPackage ../development/ocaml-modules/stdlib-random { };
+
         stdlib-shims = callPackage ../development/ocaml-modules/stdlib-shims { };
 
         stdune = callPackage ../development/ocaml-modules/stdune { };
