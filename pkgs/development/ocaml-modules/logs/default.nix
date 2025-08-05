6addce84e1268dc9f58784f54d9b017440394a01
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-08-05 22:15:56 +0700

    ocamlPackages.logs: remove problematic pname usage
    I’m sure SuperSandro2000 would be happy

--- pkgs/development/ocaml-modules/logs/default.nix
+++ pkgs/development/ocaml-modules/logs/default.nix
@@ -16,21 +16,17 @@
   lwtSupport ? true,
   cmdlinerSupport ? true,
 }:
-let
-  pname = "logs";
-  webpage = "https://erratique.ch/software/${pname}";
-in
 
 if lib.versionOlder ocaml.version "4.03" then
   throw "logs is not available for OCaml ${ocaml.version}"
 else
 
   stdenv.mkDerivation rec {
-    name = "ocaml${ocaml.version}-${pname}-${version}";
+    name = "ocaml${ocaml.version}-logs-${version}";
     version = "0.8.0";
 
     src = fetchurl {
-      url = "${webpage}/releases/${pname}-${version}.tbz";
+      url = "https://erratique.ch/software/logs/releases/logs-${version}.tbz";
       hash = "sha256-mmFRQJX6QvMBIzJiO2yNYF1Ce+qQS2oNF3+OwziCNtg=";
     };
 
@@ -45,7 +41,7 @@ else
       topkg
     ]
     ++ lib.optional cmdlinerSupport cmdliner
-    ++ lib.optional fmtSupport
+    ++ lib.optional fmtSupport fmt
     ++ lib.optional jsooSupport js_of_ocaml-compiler
     ++ lib.optional lwtSupport lwt;
 
@@ -73,7 +69,7 @@ else
 
     meta = with lib; {
       description = "Logging infrastructure for OCaml";
-      homepage = webpage;
+      homepage = "https://erratique.ch/software/logs";
       inherit (ocaml.meta) platforms;
       maintainers = [ maintainers.sternenseemann ];
       license = licenses.isc;
