98f7e70ee4f0fbe6daa0f6c035c959d8039c69f8
·𐑑𐑴𐑕𐑑𐑩𐑤 <toastal@posteo.net>
2025-08-05 22:17:24 +0700

    ocamlPackages.logs: ocamlPackages.logs: refactor to use buildTopkg

--- pkgs/development/ocaml-modules/logs/default.nix
+++ pkgs/development/ocaml-modules/logs/default.nix
@@ -3,75 +3,59 @@
   stdenv,
   fetchurl,
   ocaml,
-  findlib,
-  ocamlbuild,
   topkg,
-  result,
-  lwt,
-  cmdliner,
-  fmt,
-  fmtSupport ? lib.versionAtLeast ocaml.version "4.08",
-  js_of_ocaml-compiler,
-  jsooSupport ? true,
-  lwtSupport ? true,
+  buildTopkgPackage,
   cmdlinerSupport ? true,
+  cmdliner,
+  fmtSupport ? lib.versionAtLeast ocaml.version "4.08",
+  fmt,
+  jsooSupport ? true,
+  js_of_ocaml-compiler,
+  lwtSupport ? true,
+  lwt,
 }:
 
-if lib.versionOlder ocaml.version "4.03" then
-  throw "logs is not available for OCaml ${ocaml.version}"
-else
+buildTopkgPackage rec {
+  pname = "logs";
+  version = "0.8.0";
 
-  stdenv.mkDerivation rec {
-    name = "ocaml${ocaml.version}-logs-${version}";
-    version = "0.8.0";
+  minimalOCamlVersion = "4.03";
 
-    src = fetchurl {
-      url = "https://erratique.ch/software/logs/releases/logs-${version}.tbz";
-      hash = "sha256-mmFRQJX6QvMBIzJiO2yNYF1Ce+qQS2oNF3+OwziCNtg=";
-    };
+  src = fetchurl {
+    url = "https://erratique.ch/software/logs/releases/logs-${version}.tbz";
+    hash = "sha256-mmFRQJX6QvMBIzJiO2yNYF1Ce+qQS2oNF3+OwziCNtg=";
+  };
 
-    nativeBuildInputs = [
-      ocaml
-      findlib
-      ocamlbuild
-      topkg
-    ];
+  buildInputs = lib.optional cmdlinerSupport cmdliner;
 
-    buildInputs = [
-      topkg
-    ]
-    ++ lib.optional cmdlinerSupport cmdliner
-    ++ lib.optional fmtSupport fmt
+  propagatedBuildInputs =
+    lib.optional fmtSupport fmt
     ++ lib.optional jsooSupport js_of_ocaml-compiler
     ++ lib.optional lwtSupport lwt;
 
-    propagatedBuildInputs = [ result ];
+  strictDeps = true;
 
-    strictDeps = true;
+  buildPhase = "${topkg.run} build ${
+    lib.escapeShellArgs [
+      "--with-cmdliner"
+      (lib.boolToString cmdlinerSupport)
 
-    buildPhase = "${topkg.run} build ${
-      lib.escapeShellArgs [
-        "--with-cmdliner"
-        (lib.boolToString cmdlinerSupport)
+      "--with-fmt"
+      (lib.boolToString fmtSupport)
 
-        "--with-fmt"
-        (lib.boolToString fmtSupport)
+      "--with-js_of_ocaml-compiler"
+      (lib.boolToString jsooSupport)
 
-        "--with-js_of_ocaml-compiler"
-        (lib.boolToString jsooSupport)
+      "--with-lwt"
+      (lib.boolToString lwtSupport)
+    ]
+  }";
 
-        "--with-lwt"
-        (lib.boolToString lwtSupport)
-      ]
-    }";
-
-    inherit (topkg) installPhase;
-
-    meta = with lib; {
-      description = "Logging infrastructure for OCaml";
-      homepage = "https://erratique.ch/software/logs";
-      inherit (ocaml.meta) platforms;
-      maintainers = [ maintainers.sternenseemann ];
-      license = licenses.isc;
-    };
-  }
+  meta = {
+    description = "Logging infrastructure for OCaml";
+    homepage = "https://erratique.ch/software/logs";
+    inherit (ocaml.meta) platforms;
+    maintainers = with lib.maintainers; [ sternenseemann ];
+    license = lib.licenses.isc;
+  };
+}
