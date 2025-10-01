32a75d5b8ea6af9e77916ea84b4eb5cf1a8ae5d3
Vincent Laporte <Vincent.Laporte@gmail.com>
2025-10-01 22:28:00 +0200

    ocp-indent

--- pkgs/development/tools/ocaml/ocp-indent/default.nix
+++ pkgs/development/tools/ocaml/ocp-indent/default.nix
@@ -10,16 +10,14 @@ buildDunePackage rec {
   version = "1.9.0";
   pname = "ocp-indent";
 
-  duneVersion = "3";
-
   src = fetchFromGitHub {
     owner = "OCamlPro";
     repo = "ocp-indent";
-    rev = version;
-    sha256 = "sha256-71dbZ8c842MYZfHad6RT0E48JlgzJSHnQgLVA5dGLv8=";
+    tag = version;
+    hash = "sha256-71dbZ8c842MYZfHad6RT0E48JlgzJSHnQgLVA5dGLv8=";
   };
 
-  minimalOCamlVersion = "4.03";
+  minimalOCamlVersion = "4.08";
 
   buildInputs = [ cmdliner ];
   propagatedBuildInputs = [ findlib ];
