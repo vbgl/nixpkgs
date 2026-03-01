a2c533d3287a51721c10a1854f9bcc0ee497a44e
Fabian Affolter <mail@fabian-affolter.ch>
2026-03-01 09:27:04 +0100

    python3Packages.pick: modernize

--- pkgs/development/python-modules/pick/default.nix
+++ pkgs/development/python-modules/pick/default.nix
@@ -6,7 +6,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "pick";
   version = "2.6.0";
   pyproject = true;
@@ -14,7 +14,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "wong2";
     repo = "pick";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-/cvnDTRS3V9mk1T0zHAqdrDeRuOrnco9UF7luy687BM=";
   };
 
@@ -27,8 +27,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module to create curses-based interactive selection list in the terminal";
     homepage = "https://github.com/wong2/pick";
-    changelog = "https://github.com/wong2/pick/releases/tag/v${version}";
-    license = with lib.licenses; [ mit ];
+    changelog = "https://github.com/wong2/pick/releases/tag/${finalAttrs.src.tag}";
+    license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
