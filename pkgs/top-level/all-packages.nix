ba26734005f4ce7c9bf47be4ca2caad56e14846e
Vincent Laporte <Vincent.Laporte@gmail.com>
2026-08-26 13:44:45 +0200

    Recurse into OCaml 5.2 & 5.3 package sets

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -4260,6 +4260,8 @@ with pkgs;
   ocaml = ocamlPackages.ocaml;
 
   ocamlPackages = recurseIntoAttrs ocaml-ng.ocamlPackages;
+  OC52 = recurseIntoAttrs ocaml-ng.ocamlPackages_5_2;
+  OC53 = recurseIntoAttrs ocaml-ng.ocamlPackages_5_3;
   ocamlPackages_latest = recurseIntoAttrs ocaml-ng.ocamlPackages_latest;
 
   ocaml-crunch = ocamlPackages.crunch.bin;
